400b21d8d06ee96fb8e50fa9f3a06609047a550a
Ranithaa28 <ranithaaravichandran@gmail.com>
2026-05-16 14:04:09 +0530

    Added docker deployment

---
+++ src/main/java/com/auction/app/HomeController.java
@@ -0,0 +1,13 @@
+package com.auction.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Auction Platform Running Successfully!";
+    }
+}
