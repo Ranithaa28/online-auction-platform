556c999743c5aee84b23dd4124c215d5dd320379
Ranithaa28 <ranithaaravichandran@gmail.com>
2026-05-16 21:26:17 +0530

    Fixed null environment variable crash

--- src/main/java/com/auction/app/AuctionAppApplication.java
+++ src/main/java/com/auction/app/AuctionAppApplication.java
@@ -1,21 +1,46 @@
 package com.auction.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AuctionAppApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-        .ignoreIfMissing()
-        .load();
-		System.setProperty("Database_URI", dotenv.get("jdbc:postgresql://localhost:5432/auctionbazaar"));
-		System.setProperty("Db_username", dotenv.get("postgres"));
-		System.setProperty("Db_password", dotenv.get("postgres"));
-		SpringApplication.run(AuctionAppApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        String dbUrl = System.getenv("jdbc:postgresql://localhost:5432/auctionbazaar");
+        String dbUser = System.getenv("postgres");
+        String dbPass = System.getenv("postgres");
+
+        if (dbUrl == null) {
+            dbUrl = dotenv.get("Database_URI");
+        }
+
+        if (dbUser == null) {
+            dbUser = dotenv.get("Db_username");
+        }
+
+        if (dbPass == null) {
+            dbPass = dotenv.get("Db_password");
+        }
+
+        if (dbUrl != null) {
+            System.setProperty("Database_URI", dbUrl);
+        }
+
+        if (dbUser != null) {
+            System.setProperty("Db_username", dbUser);
+        }
+
+        if (dbPass != null) {
+            System.setProperty("Db_password", dbPass);
+        }
+
+        SpringApplication.run(AuctionAppApplication.class, args);
+    }
 }
