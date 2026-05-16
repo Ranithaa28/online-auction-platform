6e7287531ffb8ca4e14fecf3696973a7bf5c48e0
Ranithaa28 <ranithaaravichandran@gmail.com>
2026-05-16 14:40:59 +0530

    Fixed dotenv missing issue

--- src/main/java/com/auction/app/AuctionAppApplication.java
+++ src/main/java/com/auction/app/AuctionAppApplication.java
@@ -9,7 +9,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class AuctionAppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotEnv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+        .ignoreIfMissing()
+        .load();
 		System.setProperty("Database_URI", dotEnv.get("Database_URI"));
 		System.setProperty("Db_username", dotEnv.get("DB_username"));
 		System.setProperty("Db_password", dotEnv.get("DB_password"));
